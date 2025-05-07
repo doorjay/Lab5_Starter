1c366ef96b23147ee16c2e46094aa729d63b0e75
doorjay <dorjekmp@gmail.com>
2025-05-06 17:50:34 -0700

    First attempt at expose additions

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,81 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+function init() 
+{
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('#expose img');
+  const audio = document.querySelector('#expose audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('#expose button');
+
+  // initialize js-confetti 
+  const jsConfetti = new jsConfetti();
+
+  // when the user picks a horn
+  hornSelect.addEventListener('change', () => 
+  {
+    const horn = hornSelect.value; // airhorn , carhorn, or partyhorn
+
+    // update pic
+    hornImage.src = 'assets/images/${horn}.svg';
+    hornImage.alt = '$hornSelect.selectedOptions[0].text} image';
+
+    // point the audio at the right file
+    audio.src = 'assests/audio/${horn}.mp3';
+
+  });
+
+  // when the volume slider moves
+  volumeSlider.addEventListener('input', () =>
+  {
+    const vol = Number(volumeSlider.value); // 0 - 100
+
+    // HTMLAudioElement.voume is 0.0-1.0
+    audio.volume = vol / 100;
+
+    // swap the volume icon
+    if (vol === 0)
+    {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+      volumeIcon.alt = 'Muted';
+    }
+    else if (vol < 33)
+    {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+      volumeIcon.alt = 'Volume level 1';
+    }
+    else if (vol < 67)
+    {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+      volumeIcon.src = 'Volume level 2';
+    }
+    else
+    {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+      volumeIcon.alt = 'Volume level 3';
+    }
+
+  });
+
+  // when the user clicks “Play Sound”…
+  playButton.addEventListener('click', () => 
+  {
+    // only play if an audio src is set
+    if (audio.src) 
+    {
+      audio.play();
+
+      // if it’s the party horn, confetti!
+      if (hornSelect.value === 'party-horn') 
+      {
+        jsConfetti.addConfetti();
+      }
+    }
+  });
+
+  // fire an initial “input” event so the icon matches the default slider position
+  volumeSlider.dispatchEvent(new Event('input'));
+
 }
